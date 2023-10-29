634fd2e116e29154a6a552dd9a8d20f06a284779
avijayachandran <146691055+avijayachandran@users.noreply.github.com>
2023-10-29 23:41:08 +0000

    Updated to drive and item collection logic

--- o365/agenda_creator.py
+++ o365/agenda_creator.py
@@ -12,6 +12,8 @@ group_ids = ['1ab26305-df89-435b-802d-4f223a037771','aa235df6-19fc-4de3-8498-202
 class AgendaCreator:
     """This class is used for creating the agenda"""
 
+    weekly_meeting_doc_lib_id :  str = '01FL2JKR56Y2GOVW7725BZO354PWSELRRZ'
+
     def _get_next_meeting_plan(self, graph_client, group_id):
         """Gets plans for the specified group_id"""
         retry_count = 0
@@ -124,37 +126,101 @@ class AgendaCreator:
                         break # do something here, like log the error
         return drive_4_group
 
-
-    def _get_weekly_meeting_channel(self, graph_client, drive_id):
-        """Gets the drive for the specified group_id"""
+    def _get_children_for_path(self, graph_client, drive_id, children, path):
+        """Gets the children for the specified path"""
         retry_count = 0
-        weekly_meeting_channel = None
+        children_for_path = None
         while retry_count < 3:
             try:
-                print(f'Getting the weekly meeting channel for drive: {drive_id}')
-                drive_item = asyncio.run(DriveHelper.get_drive_item(graph_client, drive_id, 3)) # 3 = Weekly Meeting Channel id
-                if drive_item:
-                    return drive_item
+                #print(f'Getting the children for path: {path}')
+                path_items = path.split('/')
+                if path_items:
+                    for child in children.value:
+                        print(f'Getting the child: {child.name}, matching with path {path_items[0]}')
+                        if child.name == path_items[0]:
+                            print(f'Getting the children for path: {child.name}')
+                            children = asyncio.run(DriveHelper.get_children_by_item(graph_client, drive_id, child.id))
+                            if len(path_items) > 1:
+                                children = self._get_children_for_path(graph_client, drive_id, children, path_items[1])
+                            #print(children)
+                return children
             except RuntimeError as e:
                 if "Event loop is closed" in str(e):
                     if retry_count < 5:
-                        time.sleep(15)
+                        time.sleep(15*retry_count)
                         retry_count = retry_count + 1
                     else:
                         print(e)
                         break # do something here, like log the error
-        return weekly_meeting_channel
+        return children_for_path
 
+    def _get_drive_item_id_for_path(self, graph_client, drive_id, item_id, path):
+        """Gets the drive item id for the specified path"""
+        retry_count = 0
+        drive_item_id = None
+        while retry_count < 3:
+            try:
+                #print(f'Getting the children for path: {path}')
+                path_items = path.split('/')
+                if path_items:
+                    children = asyncio.run(DriveHelper.get_children_by_item(graph_client, drive_id, item_id))
+                    for child in children.value:
+                        print(f'Getting the child: {child.name}, matching with path {path_items[0]}')
+                        if child.name == path_items[0]:
+                            print(f'The item id for {child.name} is {child.id}')
+                            if len(path_items) > 1:
+                                drive_item_id = self._get_drive_item_id_for_path(graph_client, drive_id, child.id, path_items[1])
+                            drive_item_id = child.id
+                        return drive_item_id
+            except RuntimeError as e:
+                if "Event loop is closed" in str(e):
+                    if retry_count < 5:
+                        time.sleep(15*retry_count)
+                        retry_count = retry_count + 1
+                    else:
+                        print(e)
+                        break # do something here, like log the error
+        return drive_item_id
+
+    def _get_drive_item_id(self, graph_client, drive_id, path):
+        """Gets the drive for the specified group_id"""
+        retry_count = 0
+        drive_item_id = None
+        while retry_count < 3:
+            try:
+                print(f'Getting the weekly meeting channel children for drive: {drive_id}')
+                root_children = asyncio.run(DriveHelper.get_children_by_path(graph_client, drive_id)) # 3 = Weekly Meeting Channel id
+                if root_children and root_children.value:
+                    for root_child in root_children.value:
+                        path_items = path.split('/')
+                        if path_items[0] == root_child.name:
+                            print(f'Getting the item id for path: {path}')
+                            drive_item_id = root_child.id
+                            if len(path_items) > 1:
+                                drive_item_id = self._get_drive_item_id_for_path(graph_client, drive_id, drive_item_id, path)
+                            return drive_item_id
+            except RuntimeError as e:
+                if "Event loop is closed" in str(e):
+                    if retry_count < 5:
+                        time.sleep(15*retry_count)
+                        retry_count = retry_count + 1
+                    else:
+                        print(e)
+                        break # do something here, like log the error
+        return drive_item_id
+    
     def create(self):
         """Get the planner tasks for next meeting"""
         print('Creating agenda')
         try:
             graph_client = AuthHelper.graph_service_client()
+            drive = self._get_drive(graph_client, 'aa235df6-19fc-4de3-8498-202b5cbe2d15')
+            print(f'Drive id: {drive.id}')
+            wmc_drive_item_id = self._get_drive_item_id(graph_client, drive.id, 'Weekly Meeting Channel')
+            print(f'Weekly Meeting Channel Drive Item Id: {wmc_drive_item_id}')
+            ma_drive_item_id = self._get_drive_item_id(graph_client, drive.id, 'Meeting Automation')
+            print(f'Meeting Automation Drive Item Id: {ma_drive_item_id}')
             for group_id in group_ids:
-                drive = self._get_drive(graph_client, group_id)
-                print(f'Drive id: {drive.id}')
-                drive_item = self._get_weekly_meeting_channel(graph_client, drive.id)
-                print(f'Drive item: {drive_item.name}')
                 plan = self._get_next_meeting_plan(graph_client, group_id)
                 if plan is None:
                     print('No matching plan found for next the meeting next week')
--- o365/drive/drive_helper.py
+++ o365/drive/drive_helper.py
@@ -1,5 +1,7 @@
 from msgraph import GraphServiceClient
+from msgraph.generated.models.folder import Folder
 from kiota_abstractions.api_error import APIError
+from urllib.parse import quote
 
 class DriveHelper:
     """This is a helper for Onedrive"""
@@ -16,12 +18,69 @@ class DriveHelper:
             print(f'Error: {e.error.message}')
 
     @staticmethod
-    # GET /groups/{group-id}/drive/items/{item-id}
-    async def get_drive_item(graph_client: GraphServiceClient, drive_id: str, item_id: str):
-        """Gets the drive item for the drive id"""
+    # GET /groups/{group-id}/drive/special/{item-id}
+    async def get_drive_special(graph_client: GraphServiceClient, drive_id: str, item_id: str):
+        """Gets the drive special for the drive id"""
         try:
-            print(f'Getting the drive item matching item id {item_id}')
-            drive_item = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(item_id).get()
-            return drive_item
+            print(f'Getting the drive special matching item id {item_id}')
+            special_item = await graph_client.drives.by_drive_id(drive_id).special.by_drive_item_id(item_id).get()
+            return special_item
+        except APIError as e:
+            print(f'Error: {e.error.message}')
+
+    @staticmethod
+    # GET /drives/{drive-id}/root:/{path-relative-to-root}:/children
+    async def get_children_by_path(graph_client: GraphServiceClient, drive_id: str, relative_path: str=None):
+        """Gets the children for the drive id matching url"""
+        try:
+            path_suffix = 'root/children'
+            if relative_path is not None:
+                path_suffix = f'root:/{quote(relative_path)}/:children'
+            url_by_path = f'https://graph.microsoft.com/v1.0/drives/{drive_id}/{path_suffix}'
+            print(f'Getting the children matching url {url_by_path}')
+            children = await graph_client.drives.with_url(url_by_path).get()
+            return children
+        except APIError as e:
+            print(f'Error: {e.error.message}')
+        return None
+
+    @staticmethod
+    # GET /drives/{drive-id}/root/children
+    async def get_children_of_root(graph_client: GraphServiceClient, drive_id: str):
+        """Gets the children of root for the drive id"""
+        try:
+            print(f'Getting the children of root matching for drive id {drive_id}')
+            children = await graph_client.drives.by_drive_id(drive_id).root.get()
+            return children
+        except APIError as e:
+            print(f'Error: {e.error.message}')
+
+    @staticmethod
+    # GET /drives/{drive-id}/items/{item-id}/children
+    async def get_children_by_item(graph_client: GraphServiceClient, drive_id: str, item_id: str):
+        """Gets the children for the drive id matching sub path"""
+        try:
+            print(f'Getting the children matching item id {item_id}')
+            children = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(item_id).children.get()
+            return children
+        except APIError as e:
+            print(f'Error: {e.error.message}')
+
+    @staticmethod
+    # GET /drives/{drive-id}/items/{parent-item-id}/children
+    async def create_folder(graph_client: GraphServiceClient, drive_id: str, parent_item_id: str, folder_name: str):
+        """Create a folder under the parent item"""
+        try:
+            print(f'Creating a folder {folder_name} under item id {parent_item_id}')
+            request_body = DriveItem(
+                name = folder_name,
+                folder = Folder(
+                ),
+                additional_data = {
+                        "@microsoft_graph_conflict_behavior" : "rename",
+                }
+            )
+            folder_result = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(parent_item_id).children.post(request_body)
+            return folder_result
         except APIError as e:
             print(f'Error: {e.error.message}')
